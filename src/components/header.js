76216b11474cc8e3eb1082575699545980c1a85f
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-24 18:11:00 -0500

    created header component

---
+++ src/components/header.js
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header
+      css={css`
+        background-color: var(--colorPrimary);
+        color: var(--colorLight);
+        padding: 1rem;
+
+        .title {
+          max-width: var(--maxWidth);
+          margin: auto;
+        }
+      `}
+    >
+      <div className="title">
+        <h1>tesDo_V2</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
